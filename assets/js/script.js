fae358104401282ba4fa6943273fda803a7fcdf8
RLC <rlcarmody@gmail.com>
2019-03-30 22:08:14 -0700

    Adding liri to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,18 +35,27 @@ const projects = {
         github: 'https://github.com/rlcarmody/Word-Guess-Game',
         link: 'https://webdevpdx.com/Word-Guess-Game/',
         description: `<p class="flow-text">A fun little word game where the player guess the Pokemon.  A single onKeyUp listener is used to fire all of the game logic</p>`
+    },
+
+    liri: {
+        title: 'LIRI Bot',
+        github: 'https://github.com/rlcarmody/liri-node-app',
+        link: 'https://github.com/rlcarmody/liri-node-app',
+        description: `<p class="flow-text">A Node CLI application for querying APIs for information about movies, concerts, and songs using OMDB, Bands in Town and Spotify APIs.  The application accepts arguments from the command line and has a fallback using the Inquirer package if the user fails to enter a valid argument</p>`
     }
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let elems = document.querySelectorAll('.carousel');
-    let instances = M.Carousel.init(elems, {padding: 50, dist: -50, onCycleTo: function () {
-        let id = document.querySelector('.active').attributes.id.value;
-        $('#activeproject-description').html(projects[id].description);
-        $('#activeproject-title').text(projects[id].title);
-        $('#activeproject-links').html(`<a class="white-text" href="${projects[id].link}" target="_blank">View App <i class="fas fa-external-link-alt"></i></a><a href="${projects[id].github}" class="white-text" target="_blank">View Source Code <i
+    let instances = M.Carousel.init(elems, {
+        padding: 50, dist: -50, onCycleTo: function () {
+            let id = document.querySelector('.active').attributes.id.value;
+            $('#activeproject-description').html(projects[id].description);
+            $('#activeproject-title').text(projects[id].title);
+            $('#activeproject-links').html(`<a class="white-text" href="${projects[id].link}" target="_blank">View App <i class="fas fa-external-link-alt"></i></a><a href="${projects[id].github}" class="white-text" target="_blank">View Source Code <i
         class="fab fa-github"></i></a>`)
-    }
+        }
     })
 })
 
