557c201bcf5f6f5f3f9b0aac98b4fecdd5cf348a
RLC <rlcarmody@gmail.com>
2019-09-25 15:54:56 -0700

    Change landing page image and font-size

--- portfolio/src/Components/Name/Name.js
+++ portfolio/src/Components/Name/Name.js
@@ -3,9 +3,9 @@ import './Name.css';
 
 export default () => {
   return (
-    <div class="name">
-      <h1><span id="firstname">Ryan</span> <span class="green-text" id="lastname">Carmody</span></h1>
-      <h3 class="subtitle">Web Developer</h3>
+    <div className="name">
+      <h1><span id="firstname">Ryan</span> <span className="green-text" id="lastname">Carmody</span></h1>
+      <h3 className="subtitle">Web Developer</h3>
     </div>
   )
 }
